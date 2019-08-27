3f407f41d4b917604d0d59274cb861c6a4d9d738
Ryan <ryan@thecpafirm.com>
2019-08-26 18:35:17 -0600

    Initial Build

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let position1 = document.querySelector('h1, hero_ban')
+
+//let position2 = document.querySelector('')
+
+myFunction = () =>
+{
+    position1.textContent="Joe's Beach side Bed & Breakfast"
+
+    let userName = prompt('What is your name?')
+
+    return 0;
+}
