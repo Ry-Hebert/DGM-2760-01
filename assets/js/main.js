c0c5db5199f3daaf3f24795dc418fece4c22e97b
Ryan <ryan@thecpafirm.com>
2019-08-27 23:03:23 -0600

    Updates to css, java, and html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 let position1 = document.querySelector('h1, hero_ban')
+let position2 = document.querySelector('h2, hero_ban')
+let position3 = document.querySelector('h3, content')
 
 //let position2 = document.querySelector('')
 
 myFunction = () =>
 {
-    position1.textContent="Joe's Beach side Bed & Breakfast"
+    position1.textContent="Joe's Beachside Bed & Breakfast"
 
-    let userName = prompt('What is your name?')
+    position2.textContent="Life can be a beach, so take some time to relax."
+
+    position3.textContent="Life can be a beach, so take some time to relax."
+
+    let userName = prompt("Joe's Beachside Bed & Breakfast would like to know: /br What is your name?")
 }
